a8fd8955d613e17e761adcfcbc910dc7471d739a
Michel John <michel.john@hotmail.com>
2022-12-06 07:39:22 -0300

    feature: handler error

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -7,5 +8,10 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, req, res, next) => {
+  console.log(error);
+
+  res.sendStatus(500);
+});
 
 app.listen(PORT, () => console.log(`🚀 Server started on http://localhost:${PORT}.`));
