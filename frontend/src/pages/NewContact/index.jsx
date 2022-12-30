16ba18072d14e4b6af8422b68e39900218bdaa8b
Michel John <michel.john@hotmail.com>
2022-12-30 13:23:59 -0300

    feat: implements mapper for contacts

--- frontend/src/pages/NewContact/index.jsx
+++ frontend/src/pages/NewContact/index.jsx
@@ -1,23 +1,16 @@
 import { useRef } from 'react';
 
 import { toast } from '../../utils/toast';
+import ContactsService from '../../services/ContactsService';
 
 import { PageHeader } from '../../components/PageHeader';
 import { ContactForm } from '../../components/ContactForm';
-import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
   const contactFormRef = useRef(null);
 
-  async function handleSubmit(formData) {
+  async function handleSubmit(contact) {
     try {
-      const contact = {
-        name: formData.name,
-        email: formData.email,
-        phone: formData.phone,
-        category_id: formData.categoryId,
-      };
-
       await ContactsService.createContact(contact);
 
       contactFormRef.current.resetFields();
